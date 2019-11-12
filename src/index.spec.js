6097e9ef3885c062ccdeaefdc686f3eee83b8b55
root <jjbillupsbrown@gmail.com>
2019-11-12 23:45:21 +0000

    deleting jest and supertest package

---
+++ src/index.spec.js
@@ -0,0 +1,30 @@
+
+const { createTestClient } = require('apollo-server-testing');
+
+it('fetches single launch', async () => {
+  const userAPI = new UserAPI({ store });
+  const launchAPI = new LaunchAPI();
+
+  // create a test server to test against, using our production typeDefs,
+  // resolvers, and dataSources.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({ userAPI, launchAPI }),
+    context: () => ({ user: { id: 1, email: 'a@a.a' } }),
+  });
+
+  // mock the dataSource's underlying fetch methods
+  launchAPI.get = jest.fn(() => [mockLaunchResponse]);
+  userAPI.store = mockStore;
+  userAPI.store.trips.findAll.mockReturnValueOnce([
+    { dataValues: { launchId: 1 } },
+  ]);
+
+  // use the test server to create a query function
+  const { query } = createTestClient(server);
+
+  // run query against the server and snapshot the output
+  const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
+  expect(res).toMatchSnapshot();
+});
