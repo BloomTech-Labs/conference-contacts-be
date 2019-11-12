22b98af4462a925be95cd1cdc387d278b1da9620
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-11-12 00:05:39 -0500

    added resolvers and mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 const Mutation = {
+  // Mutation User
   createUser(parent, args, { prisma }, info) {
     return prisma.createUser(args);
   },
@@ -10,7 +11,77 @@ const Mutation = {
   },
   deleteUser(parent, { id }, { prisma }, info) {
     return prisma.deleteUser({ id });
-  }
+  },
+
+  // Mutation Connection
+  createConnection(parent, args, { prisma }, info){
+    return prisma.createConnection(args)
+  },
+  updateConnection(parent, { id, ...changes }, { prisma }, info){
+    return prisma.updateConnection({
+      data: changes,
+      where: { id }
+    })
+  },
+  deleteConnection(parent, { id }, { prisma }, info){
+    return prisma.deleteConnection({ id })
+  },
+
+  // Mutation Manual Contact
+  createManualContact(parent, args, { prisma }, info){
+    return prisma.createManualContact(args)
+  },
+  updateManualContact(parent, { id, ...changes }, { prisma }, info){
+    return prisma.updateManualContacts({
+      data: changes,
+      where: { id }
+    })
+  },
+  deleteManualContact(parent, { id }, { prisma }, info){
+    return prisma.deleteManualContact({ id })
+  },
+
+  // Mutation Profile Field
+  createProfileField(parent, args, { prisma }, info){
+    return prisma.createProfileField(args)
+  },
+  updateProfileField(parent, { id, ...changes }, { prisma }, info){
+    return prisma.updateProfileField({
+      data: changes,
+      where: { id }
+    })
+  },
+  deleteProfileField(parent, { id }, { prisma }, info){
+    return this.deleteProfileField({ id })
+  },
+
+  // Mutation Conference
+  createConference(parent, args, { prisma }, info){
+    return prisma.createConference(args)
+  },
+  updateConference(parent, { id, ...changes }, { prisma }, info){
+    return prisma.updateConference({
+      data: changes,
+      where: { id }
+    })
+  },
+  deleteConference(parent, { id }, { prisma }, info){
+    return this.deleteConference({ id })
+  },
+
+  // Mutation Coordinate
+  createCoordinate(parent, args, { prisma }, info){
+    return prisma.createCoordinate(args)
+  },
+  updateCoordinate(parent, { id, ...changes }, { prisma }, info){
+    return prisma.updateCoordinate({
+      data: changes,
+      where: { id }
+    })
+  },
+  deleteCoordinate(parent, { id }, { prisma }, info){
+    return this.deleteCoordinate({ id })
+  },
 };
 
 module.exports = Mutation;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,58 @@
 const Query = {
+  
+  // Users
   users(parent, args, { prisma }, info) {
     return prisma.users();
   },
+
+  // User
   user(parent, { email }, { prisma }, info) {
     return prisma.user({ email });
+  },
+
+  // Manual Contact
+  ManualContact(parent, args, { prisma }, info){
+    return prisma.ManualContact();
+  },
+
+  // Profile Field
+  ProfileField(parent, args, { prisma }, info){
+    return prisma.ProfileField();
+  },
+
+  // Connection
+  UsersConnection(parent, { id }, { prisma }, info){
+    return prisma.UsersConnection({ id });
+  },
+
+  // Conferences
+  conferences(parent, { id }, { prisma }, info){
+    return prisma.conferences({ id });
+  },
+
+  // Conference
+  conference(parent, { id }, { prisma }, info){
+    return prisma.conference({ id });
+  },
+
+  // Coordinate
+  Coordinate(parent, args, { prisma }, info){
+    return primsa.Coordinate();
+  },
+
+  // Connection Status
+  ConnectionStatus(parent, args, { prisma }, info){
+    return prisma.ConnectionStatus()
+  },
+
+  // Info
+  Info(parent, args, { prisma }, info){
+    return prisma.Info()
+  },
+
+  // Privacy 
+  Privacy(parent, args, { prisma }, info){
+    return prisma.Privacy()
   }
 };
 
