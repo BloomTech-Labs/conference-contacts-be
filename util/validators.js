e64209aa202491c1fe1692185ff68042d3a72a91
zachary-peasley <zpman95@hotmail.com>
2019-11-12 23:31:04 -0600

    validators add

--- util/validators.js
+++ util/validators.js
@@ -27,3 +27,17 @@ module.exports.validateRegisterInput = (
     };
   };
   
+  module.exports.validateLoginInput = (username, password) => {
+    const errors = {};
+    if (username.trim() === '') {
+      errors.username = 'Username must not be empty';
+    }
+    if (password.trim() === '') {
+      errors.password = 'Password must not be empty';
+    }
+  
+    return {
+      errors,
+      valid: Object.keys(errors).length < 1
+    };
+  };
