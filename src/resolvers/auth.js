809cf1c299aa13765efa0b793955ae8dcfebc4b7
zachary-peasley <zpman95@hotmail.com>
2019-11-12 23:31:59 -0600

    Working on auth login

---
+++ src/resolvers/auth.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
+
+const {
+  validateRegisterInput,
+  validateLoginInput
+} = require('../../util/validators');
+const { SECRET_KEY } = require('../../config');
+const User = require('../../model');
+
