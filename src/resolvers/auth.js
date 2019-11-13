8cc6fe5c6879b36d414c74296e84e81c399e24f9
zachary-peasley <zpman95@hotmail.com>
2019-11-12 23:32:54 -0600

    generateToken

--- src/resolvers/auth.js
+++ src/resolvers/auth.js
@@ -9,3 +9,46 @@ const {
 const { SECRET_KEY } = require('../../config');
 const User = require('../../model');
 
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+}
+
+module.exports = {
+  Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = 'User not found';
+        throw new UserInputError('User not found', { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong crendetials';
+        throw new UserInputError('Wrong crendetials', { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      };
+    },
+  }}
