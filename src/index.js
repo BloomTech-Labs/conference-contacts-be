c1ebdc557bb557318531e65352f1d82e7dad33ba
macjabeth <macbethjonathan@gmail.com>
2019-11-18 19:27:24 -0500

    add environment port

--- src/index.js
+++ src/index.js
@@ -68,6 +68,6 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
