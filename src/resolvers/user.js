8cefb360de39f70b95f702a3b41a76fea3747e5e
Bobby Leon Hall Jr <bobbyhalljrcs@gmail.com>
2020-05-18 17:05:49 -0400

    Revert "Staging"

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,18 +1,5 @@
 const User = {
-  async profile(
-    { id },
-    _,
-    {
-      dataSources: { prisma },
-      user
-    }
-  ) {
-    // public query, goes around the need for authentication
-    if (!user) {
-      const userProfile = await prisma.user({ id }).profile();
-      return userProfile;
-    }
-
+  async profile({ id }, _, { dataSources: { prisma }, user }) {
     const userProfile = await prisma.user({ id }).profile();
 
     // return profile data for the current logged in user if
@@ -44,22 +31,10 @@ const User = {
       )
     );
   },
-  qrcodes(
-    { id },
-    _,
-    {
-      dataSources: { prisma }
-    }
-  ) {
+  qrcodes({ id }, _, { dataSources: { prisma } }) {
     return prisma.user({ id }).qrcodes();
   },
-  async connections(
-    { id },
-    _,
-    {
-      dataSources: { prisma }
-    }
-  ) {
+  async connections({ id }, _, { dataSources: { prisma } }) {
     const connections = await prisma.connections({
       where: {
         OR: [{ sender: { id } }, { receiver: { id } }]
@@ -67,31 +42,13 @@ const User = {
     });
     return connections.filter(connection => connection.status === 'CONNECTED');
   },
-  sentConnections(
-    { id },
-    _,
-    {
-      dataSources: { prisma }
-    }
-  ) {
+  sentConnections({ id }, _, { dataSources: { prisma } }) {
     return prisma.user({ id }).sentConnections();
   },
-  receivedConnections(
-    { id },
-    _,
-    {
-      dataSources: { prisma }
-    }
-  ) {
+  receivedConnections({ id }, _, { dataSources: { prisma } }) {
     return prisma.user({ id }).receivedConnections();
   },
-  async pendingConnections(
-    { id },
-    _,
-    {
-      dataSources: { prisma }
-    }
-  ) {
+  async pendingConnections({ id }, _, { dataSources: { prisma } }) {
     const connections = await prisma.connections({
       where: {
         OR: [{ sender: { id } }, { receiver: { id } }]
@@ -99,22 +56,10 @@ const User = {
     });
     return connections.filter(connection => connection.status === 'PENDING');
   },
-  blockedConnections(
-    { id },
-    _,
-    {
-      dataSources: { prisma }
-    }
-  ) {
+  blockedConnections({ id }, _, { dataSources: { prisma } }) {
     return prisma.user({ id }).blockedConnections();
   },
-  notifications(
-    { id },
-    _,
-    {
-      dataSources: { prisma }
-    }
-  ) {
+  notifications({ id }, _, { dataSources: { prisma } }) {
     return prisma.user({ id }).notifications();
   }
 };
